16a591dfdd3426afc6a4cc6c6791ecd337383cb8
theludovyc <theludovyc@gmail.com>
2020-12-20 20:16:26 +0100

    update

--- Script/Player.gd
+++ Script/Player.gd
@@ -8,47 +8,40 @@ var dirX := 0
 var dirY := 0
 
 var x_speed := 0.0
-const X_ACC = 1.0
+const X_ACC = 1.8
 const MAX_X_SPEED = 200.0
 
-const FALL_SPEED = 300.0
+const FALL_SPEED = 310.0
 
 var wantJump := false
-var jumping := false
 var y_speed := 0.0
-const Y_DEC = 600.0
-const MAX_JUMP_SPEED = 275.0
+const MAX_JUMP_SPEED = 220.0
+
+var inputs:PoolStringArray
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	inputs = ["ui_right", "ui_left", "ui_up"]
 	pass # Replace with function body.
 
 func _process(delta):
-	dirX = int(Input.is_action_pressed("ui_right"))-int(Input.is_action_pressed("ui_left"))
+	dirX = int(Input.is_action_pressed(inputs[0]))-int(Input.is_action_pressed(inputs[1]))
 	
 	if dirX != 0:
 		x_speed = lerp(x_speed, dirX * MAX_X_SPEED, X_ACC)
 	else:
 		x_speed = lerp(x_speed, 0, X_ACC)
 			
-	wantJump = Input.is_action_just_pressed("ui_up") 
+	wantJump = Input.is_action_just_pressed(inputs[2])
 	
 func _physics_process(delta):
-	if jumping:
-		$Label.text = "jumping"
-		y_speed += Y_DEC * delta
-		
-		if y_speed > 0:
-			y_speed = 0
-			jumping = false
-	elif $RayCast2D.is_colliding():
+	if $RayCast2D.is_colliding():
 		$Label.text = "on ground"
 		y_speed = 0
 		
 		if wantJump:
 			$Label.text = "start jump"
 			y_speed = -MAX_JUMP_SPEED
-			jumping = true
 	else:
 		$Label.text = "falling"
 		y_speed += FALL_SPEED * delta
---
+++ Script/Player2.gd
@@ -0,0 +1,4 @@
+extends "res://Script/Player.gd"
+
+func _ready():
+	inputs = ["p2_right", "p2_left", "p2_up"]
