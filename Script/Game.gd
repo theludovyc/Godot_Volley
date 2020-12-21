39dbac7e98b33e2a52c8c02f64c23a52f0e2bc4a
theludovyc <theludovyc@gmail.com>
2020-12-21 23:12:59 +0100

    update

--- Script/Game.gd
+++ Script/Game.gd
@@ -5,6 +5,7 @@ extends Node
 # var a = 2
 # var b = "text"
 var Ball = preload("res://Scene/Ball.tscn")
+var ball
 
 var scores:PoolIntArray
 
@@ -14,42 +15,35 @@ var lastWin := false
 
 var win := false
 
+var counter := 3
+
+func spawnBall():
+	ball = Ball.instance()
+	
+	if !lastWin:
+		ball.position = Vector2(225, 400)
+	else:
+		ball.position = Vector2(710, 400)
+		
+	ball.connect("body_entered", self, "_on_Ball_body_entered")
+	call_deferred("add_child", ball)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	scores = [0, 0]
 	
 	scoreNodes = [$ScoreP1, $ScoreP2]
 	
-	$Player.myPause(false)
-	$Player2.myPause(false)
+	spawnBall()
 	
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if !win and Input.is_action_just_pressed("ui_accept"):
-		var ball = Ball.instance()
-		
-		if !lastWin:
-			ball.position = Vector2(225, 416)
-		else:
-			ball.position = Vector2(710, 416)
-			
-		ball.connect("body_entered", self, "_on_Ball_body_entered")
-		add_child(ball)
-	
-		$Player.myPause(true)
-		$Player2.myPause(true)
-		
-		win = true
-
-func freeBall():
-	$Ball.queue_free()
+#func _process(delta):
 	
 #	pass
 func reset(var i, var b):
-	call_deferred("freeBall")
+	ball.queue_free()
 			
 	scores[i] += 1
 	
@@ -57,10 +51,17 @@ func reset(var i, var b):
 	
 	lastWin = b
 	
-	$Player.myPause(false)
-	$Player2.myPause(false)
+	counter = 3
+	
+	var label = $LabelTimer
+	label.text = str(counter)
+	label.show()
 	
 	win = false
+	
+	spawnBall()
+	
+	$Timer.start()
 
 func _on_Ball_body_entered(body):
 	match(body.name):
@@ -73,4 +74,21 @@ func _on_Ball_body_entered(body):
 			print("player2 lose")
 			
 			reset(0, false)
+	pass # Replace with function body.
+
+
+func _on_Timer_timeout():
+	if counter > 1:
+		counter -= 1
+		
+		$LabelTimer.text = str(counter)
+	else:
+		win = true
+		
+		ball.mode = RigidBody2D.MODE_RIGID
+		
+		$LabelTimer.hide()
+		
+		$Timer.stop()
+	
 	pass # Replace with function body.
